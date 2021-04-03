fcd620c243b52e2f31249e0e68951cfa1207222f
Victor Santana <isai@raldoo.com>
2021-04-03 13:37:39 -0600

    gitignore fixed

--- storage/framework/views/51decaeb84db3599746580920211c10e7f1140b9.php
+++
@@ -1,3 +0,0 @@
-<!-- begin #page-loader -->
-<div id="page-loader" class="fade show"><span class="spinner"></span></div>
-<!-- end #page-loader --><?php /**PATH C:\xampp\htdocs\DMPI\so-politica\api\resources\views/includes/component/page-loader.blade.php ENDPATH**/ ?>
--- storage/framework/views/8b0038c7cbe011a7888f14e1d1fa572b5a10d0f3.php
+++
@@ -1,4 +0,0 @@
-<!-- begin scroll to top btn -->
-<a href="javascript:;" class="btn btn-icon btn-circle btn-success btn-scroll-to-top fade" data-click="scroll-top"><i class="fa fa-angle-up"></i></a>
-<!-- end scroll to top btn -->
-<?php /**PATH C:\xampp\htdocs\DMPI\so-politica\resources\views/includes/component/scroll-top-btn.blade.php ENDPATH**/ ?>
